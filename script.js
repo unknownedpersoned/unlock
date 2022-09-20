4944934e1888d51d52f4ea3531cf41b063e9e06e
Leo Markley <stencylxd@gmail.com>
2022-09-20 13:01:37 -0400

    Formatting

--- script.js
+++ script.js
@@ -2,28 +2,27 @@ var textbox = document.getElementById("textbox");
 var frame = document.getElementById("frame");
 frame.style.filter = "blur(3px)";
 
-textbox.addEventListener('keydown', function (e){
-		if (e.code == "Space") {
-			e.preventDefault();
-			frame.setAttribute("src", "https://g.deev.is/eaglercraft/");
-			transition(); 
-		}
+textbox.addEventListener('keydown', function(e) {
+ 	if (e.code == "Space") {
+		e.preventDefault();
+		frame.setAttribute("src", "https://g.deev.is/eaglercraft/");
+		transition(); 
+ 	}
 	
-		if (e.code == "Enter") {
-			e.preventDefault();
-			if (!textbox.value == "") {
-				if (!textbox.value.startsWith("https://"))
-					textbox.value = "https://" + textbox.value;
-				frame.setAttribute("src", textbox.value);
-				transition();
-			}
+	if (e.code == "Enter") {
+		e.preventDefault();
+		if (!textbox.value == "") {
+			if (!textbox.value.startsWith("https://")) textbox.value = "https://" + textbox.value;
+			frame.setAttribute("src", textbox.value);
+			transition();
 		}
+	}
 }, false);
 
 function transition() {
 	textbox.style.opacity = "0";
-  textbox.style.visibility = "hidden";
-	setTimeout(function(){
+	textbox.style.visibility = "hidden";
+	setTimeout(function() {
 		frame.style.opacity = "1";
 		frame.style.filter = "none";
 	}, 800);
